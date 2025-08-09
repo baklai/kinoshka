c6c3c97b63521451b98765f968ffbd5e645d6304
Dmitrii Baklai <d.n.baklai@gmail.com>
2025-08-09 10:45:31 +0300

    fix: rebuid project

--- constants/Categories.ts
+++
@@ -1,9 +0,0 @@
-import { CategoryProps } from '@/types/category.type';
-
-const categories: CategoryProps[] = [
-  { name: 'filmy', description: 'Фільми', icon: 'movie' },
-  { name: 'seriesss', description: 'Серіали', icon: 'movie' },
-  { name: 'cartoon', description: 'Мультфільми', icon: 'movie' }
-];
-
-export default categories;
--- types/category.type.ts
+++
@@ -1,9 +0,0 @@
-import { MaterialIcons } from '@expo/vector-icons';
-
-type MaterialIconName = keyof typeof MaterialIcons.glyphMap;
-
-export interface CategoryProps {
-  name: string;
-  description: string;
-  icon: MaterialIconName;
-}
