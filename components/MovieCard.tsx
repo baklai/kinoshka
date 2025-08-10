8165c7d0405e19b1295d647789ac6e435354c12b
Dmitrii Baklai <d.n.baklai@gmail.com>
2025-08-10 16:24:22 +0300

    fix: update card

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -14,7 +14,7 @@ export default function MovieCard(
 
   return (
     <TouchableOpacity
-      activeOpacity={0.9}
+      activeOpacity={1}
       style={[styles.container, focused && styles.containerFocused]}
       onFocus={() => setFocused(true)}
       onBlur={() => setFocused(false)}
@@ -29,6 +29,17 @@ export default function MovieCard(
           transition={1000}
         />
 
+        <View style={styles.overlayTop}>
+          <View style={styles.rating}>
+            <MaterialIcons name="recommend" size={scaledPixels(14)} color="#c5c5c5" />
+            <Text style={styles.ratingText}>{rating}</Text>
+          </View>
+
+          <View style={styles.quality}>
+            <Text style={styles.qualityText}>1080p</Text>
+          </View>
+        </View>
+
         {focused && (
           <MaterialIcons
             name="play-circle"
@@ -39,21 +50,7 @@ export default function MovieCard(
           />
         )}
 
-        <View style={styles.overlayTop}>
-          <View style={styles.rating}>
-            <MaterialIcons
-              name="recommend"
-              size={scaledPixels(14)}
-              color="#c5c5c5"
-              style={styles.icon}
-            />
-            <Text style={styles.ratingText}>{rating}</Text>
-          </View>
-
-          <View style={styles.quality}>
-            <Text style={styles.qualityText}>1080p</Text>
-          </View>
-        </View>
+        {focused && <View style={styles.overlay} />}
       </View>
 
       <View style={styles.overlayBottom}>
@@ -85,7 +82,7 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
     height: '100%',
-    backgroundColor: '#0553'
+    backgroundColor: '#0E0E0F'
   },
   playIcon: {
     position: 'absolute',
@@ -96,7 +93,6 @@ const styles = StyleSheet.create({
     marginTop: -scaledPixels(40),
     zIndex: 10
   },
-  icon: {},
   overlayTop: {
     display: 'flex',
     flexDirection: 'row',
@@ -104,7 +100,8 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: scaledPixels(8),
     left: scaledPixels(4),
-    right: scaledPixels(4)
+    right: scaledPixels(4),
+    zIndex: 10
   },
   overlayBottom: {
     display: 'flex',
@@ -120,7 +117,7 @@ const styles = StyleSheet.create({
     right: 0
   },
   rating: {
-    backgroundColor: '#19875470',
+    backgroundColor: '#19875480',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
@@ -133,7 +130,8 @@ const styles = StyleSheet.create({
   },
   ratingText: {
     color: '#fff',
-    fontSize: scaledPixels(12)
+    fontSize: scaledPixels(14),
+    paddingHorizontal: scaledPixels(3)
   },
   quality: {
     backgroundColor: '#00000070',
@@ -145,154 +143,18 @@ const styles = StyleSheet.create({
   },
   qualityText: {
     color: '#fff',
-    fontSize: scaledPixels(12)
+    fontSize: scaledPixels(14)
   },
   title: {
     color: '#fff',
-    fontSize: scaledPixels(14),
-    fontWeight: '600',
+    fontSize: scaledPixels(18),
+    fontWeight: 'bold',
     textAlign: 'center',
     textShadowOffset: { width: 0, height: 1 },
     textShadowRadius: 1
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.6)'
   }
 });
-
-// import { BLUR_HASH_MOVIE_CARD } from '@/constants/Blurhash';
-// import { scaledPixels } from '@/hooks/useScaledPixels';
-// import { MovieProps } from '@/types/movie.type';
-// import { MaterialIcons } from '@expo/vector-icons';
-// import { Image } from 'expo-image';
-// import { useState } from 'react';
-// import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-// export default function MovieCard(
-//   props: MovieProps & { handlePress?: (item: MovieProps) => void }
-// ) {
-//   const { poster, title, rating, handlePress } = props;
-//   const [focused, setFocused] = useState(false);
-
-//   return (
-//     <TouchableOpacity
-//       focusable
-//       activeOpacity={0.8}
-//       style={[styles.container, focused && styles.containerFocused]}
-//       onFocus={() => setFocused(true)}
-//       onBlur={() => setFocused(false)}
-//       onPress={() => handlePress && handlePress(props)}
-//     >
-//       <View style={styles.imageWrapper}>
-//         <Image
-//           style={styles.image}
-//           source={poster}
-//           placeholder={{ blurhash: BLUR_HASH_MOVIE_CARD }}
-//           contentFit="cover"
-//           transition={1000}
-//         />
-
-//         <View style={styles.overlayTop}>
-//           <View style={styles.rating}>
-//             <MaterialIcons
-//               name="recommend"
-//               size={scaledPixels(14)}
-//               color="#c5c5c5"
-//               style={styles.icon}
-//             />
-//             <Text style={styles.ratingText}>{rating}</Text>
-//           </View>
-
-//           <View style={styles.quality}>
-//             <Text style={styles.qualityText}>1080p</Text>
-//           </View>
-//         </View>
-//       </View>
-//       <View style={styles.overlayBottom}>
-//         <Text style={styles.title}>{title}</Text>
-//       </View>
-//     </TouchableOpacity>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     borderRadius: scaledPixels(11),
-//     alignItems: 'center',
-//     borderWidth: scaledPixels(3),
-//     borderColor: 'transparent',
-//     margin: scaledPixels(6)
-//   },
-//   containerFocused: {
-//     borderColor: '#ca563f'
-//   },
-//   imageWrapper: {
-//     width: scaledPixels(200),
-//     height: scaledPixels(300),
-//     borderRadius: scaledPixels(8),
-//     overflow: 'hidden',
-//     position: 'relative'
-//   },
-//   image: {
-//     flex: 1,
-//     width: '100%',
-//     height: '100%',
-//     backgroundColor: '#0553'
-//   },
-//   icon: {},
-//   overlayTop: {
-//     display: 'flex',
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//     position: 'absolute',
-//     top: scaledPixels(8),
-//     left: scaledPixels(4),
-//     right: scaledPixels(4)
-//   },
-//   overlayBottom: {
-//     display: 'flex',
-//     flexDirection: 'row',
-//     justifyContent: 'center',
-//     position: 'absolute',
-//     backgroundColor: '#33333370',
-//     borderBottomLeftRadius: scaledPixels(3),
-//     borderBottomRightRadius: scaledPixels(3),
-//     paddingVertical: scaledPixels(4),
-//     bottom: 0,
-//     left: 0,
-//     right: 0
-//   },
-//   rating: {
-//     backgroundColor: '#19875470',
-//     display: 'flex',
-//     flexDirection: 'row',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     paddingHorizontal: scaledPixels(4),
-//     paddingVertical: scaledPixels(2),
-//     borderRadius: scaledPixels(4),
-//     marginBottom: scaledPixels(4),
-//     gap: scaledPixels(2)
-//   },
-//   ratingText: {
-//     color: '#fff',
-//     fontSize: scaledPixels(12)
-//   },
-//   quality: {
-//     backgroundColor: '#00000070',
-//     alignSelf: 'flex-start',
-//     paddingHorizontal: scaledPixels(6),
-//     paddingVertical: scaledPixels(2),
-//     borderRadius: scaledPixels(4),
-//     marginBottom: scaledPixels(4)
-//   },
-//   qualityText: {
-//     color: '#fff',
-//     fontSize: scaledPixels(12)
-//   },
-//   title: {
-//     color: '#fff',
-//     fontSize: scaledPixels(14),
-//     fontWeight: '600',
-//     textAlign: 'center',
-//     textShadowOffset: { width: 0, height: 1 },
-//     textShadowRadius: 1
-//   }
-// });
