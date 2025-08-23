aa2183f18923b39a7dc2a73dbe339af2b3642b86
Dmitrii Baklai <d.n.baklai@gmail.com>
2025-08-23 13:03:48 +0300

    feat: улучшить экран IndexScreen с добавлением загрузчика и обновить стиль TVFocusGuideView fix: добавить недостающие символы в константу KEYBOARD

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
 import MoviesFlatList from '@/components/MoviesFlatList';
-import React from 'react';
-import { ScrollView, StyleSheet, TVFocusGuideView } from 'react-native';
+import { AppTheme } from '@/constants/theme.constant';
+import { useAsyncFetch } from '@/hooks/useAsyncFetch';
+import React, { useEffect } from 'react';
+import { ActivityIndicator, ScrollView, StyleSheet, TVFocusGuideView } from 'react-native';
 
 export default function IndexScreen() {
+  const { loading, findAll } = useAsyncFetch('');
+
   const genres = [['Фільми'], ['Серіали'], ['Мультфільми']];
 
+  useEffect(() => {
+    findAll();
+  }, []);
+
+  if (loading) {
+    return (
+      <ActivityIndicator size="large" color={AppTheme.colors.primary} style={styles.container} />
+    );
+  }
+
   return (
-    <TVFocusGuideView style={{ flex: 1 }} trapFocusLeft trapFocusRight trapFocusDown>
+    <TVFocusGuideView style={styles.container} trapFocusLeft trapFocusRight trapFocusDown>
       <ScrollView
         style={styles.container}
         showsVerticalScrollIndicator={false}
