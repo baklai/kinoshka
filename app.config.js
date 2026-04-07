cde6a5c10b05a68c72afd47ba4346e0b3c2037af
Dmytro Baklai <d.n.baklai@gmail.com>
2026-04-07 15:51:37 +0300

    fix: update config

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'KinoshkaTV',
     slug: 'KinoshkaTV',
-    version: process.env.EXPO_PUBLIC_APP_VERSION || '0.0.1',
+    version: process.env.EXPO_PUBLIC_APP_VERSION || '0.0.0',
     orientation: 'default',
     icon: './assets/images/icon.png',
     userInterfaceStyle: 'dark',
@@ -105,62 +105,7 @@ module.exports = {
         }
       ],
       'expo-sharing',
-      'expo-file-system',
-      function withM3uIntentSupport(config) {
-        const { withAndroidManifest } = require('@expo/config-plugins');
-        return withAndroidManifest(config, async config => {
-          const manifest = config.modResults;
-          const app = manifest.manifest;
-
-          if (!app.queries) app.queries = [];
-          const existingMimeTypes = app.queries
-            .flatMap(q => q.intent || [])
-            .flatMap(i => i.data || [])
-            .map(d => d.$?.['android:mimeType']);
-
-          const mimeTypesToAdd = ['application/vnd.apple.mpegurl', 'audio/x-mpegurl', 'video/*'];
-          for (const mimeType of mimeTypesToAdd) {
-            if (!existingMimeTypes.includes(mimeType)) {
-              app.queries.push({
-                intent: [
-                  {
-                    action: [{ $: { 'android:name': 'android.intent.action.VIEW' } }],
-                    data: [{ $: { 'android:mimeType': mimeType } }]
-                  }
-                ]
-              });
-            }
-          }
-          if (!app.application) app.application = [{}];
-          const application = app.application[0];
-          if (!application.provider) application.provider = [];
-
-          const providerName = 'expo.modules.filesystem.FileSystemFileProvider';
-          const alreadyAdded = application.provider.some(
-            p => p.$?.['android:name'] === providerName
-          );
-          if (!alreadyAdded) {
-            application.provider.push({
-              $: {
-                'android:name': providerName,
-                'android:authorities': '${applicationId}.FileSystemFileProvider',
-                'android:exported': 'false',
-                'android:grantUriPermissions': 'true'
-              },
-              'meta-data': [
-                {
-                  $: {
-                    'android:name': 'android.support.FILE_PROVIDER_PATHS',
-                    'android:resource': '@xml/file_system_provider_paths'
-                  }
-                }
-              ]
-            });
-          }
-
-          return config;
-        });
-      }
+      'expo-file-system'
     ],
     experiments: {
       typedRoutes: true,
